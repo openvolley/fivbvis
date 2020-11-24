a513858f3a178935b34e0ca26ca48ba1a39577a1
Tyler Widd <51216700+tyler-widdison@users.noreply.github.com>
2020-11-24 08:30:49 -0700

    initial event get

--- R/event.R
+++ R/event.R
@@ -1,2 +1,46 @@
-## TODO https://www.fivb.org/VisSDK/VisWebService/#GetEvent.html
-## TODO https://www.fivb.org/VisSDK/VisWebService/#GetEventList.html
+#' Get a event
+#'
+#' @references \url{https://www.fivb.org/VisSDK/VisWebService/#GetEvent.html}
+#' @param no integer: the number of event
+#' @param fields character: fields to return
+#'
+#' @return A data.frame
+#'
+#' @examples
+#' \dontrun{
+#'   v_get_event(1)
+#' }
+#'
+#' @export
+v_get_event <- function(no, fields) {
+  ##  <Request Type="GetEvent"
+  ##           No="<event number>"
+  ##           Fields="<optional: list of the fields to return>" />
+  req <- v_request(type = "GetEvent", no = no, fields = fields)
+  out <- make_request(req, node_path = "//Event")
+  out <- v_remap(out, col = "OrganizerType", schema = "Organizer Type")
+  v_remap(out, col = "Type", schema = "Event Type")
+}
+
+
+#' Get a event list
+#'
+#' @references \url{https://www.fivb.org/VisSDK/VisWebService/#GetEventList.html}
+#' @param fields character: fields to return
+#'
+#' @return A data.frame
+#'
+#' @examples
+#' \dontrun{
+#'   v_get_event_list()
+#' }
+#'
+#' @export
+v_get_event_list <- function(fields = v_fields("Event"), version, filter) {
+  ##  <Request Type="GetEventList"
+  ##           Fields="<list of the fields to return>">
+  ##    <Filter /> <!-- Optional: contains the filter to use -->
+  ##  </Request>
+  req <- v_request(type = "GetEventList", fields = fields, version = version, filter = filter)
+  make_request(request = req, node_path = "//Event")
+}
